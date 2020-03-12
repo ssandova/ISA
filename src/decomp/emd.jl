18199bb5c7eb2719a6aa5f6f64f22976b60bd7af
Steven Sandoval <ssandova@gmail.com>
2020-03-11 20:38:47 -0600

    Added Error checking and made many minor performance improvements

--- src/decomp/emd.jl
+++ src/decomp/emd.jl
@@ -25,64 +25,78 @@ function findLocalMaxima(signal::Vector; angle=0.0,  includeEdge = :false)
     end
   end
   Vals = [ signal[idx[1]] for idx in Inds]
-  return IndsVals = [Inds, Vals]
+  return IndsVals = (Inds, Vals)
 end
 
 function SIFT(r::Vector{Float64}; siftStopThresh = 1e-6, includeEdge = :true)
   L = length(r)
+  eStart = round(Int,L/4); eStop = round(Int,3L/4); eL = length(eStart:eStop)
   while true
     maxIndsVals = findLocalMaxima(r, includeEdge=includeEdge)
+    if length(maxIndsVals[1]) < 4; break; end
     maxInterpolator = Dierckx.Spline1D(maxIndsVals[1], maxIndsVals[2])
     u = maxInterpolator(1:L)
     minIndsVals = findLocalMaxima(-r, includeEdge=includeEdge)
+    if length(minIndsVals[1]) < 4; break; end
     minInterpolator = Dierckx.Spline1D(minIndsVals[1], -minIndsVals[2])
     l = minInterpolator(1:L)
     e = (u+l)./2
     r -= e
-    mae = sum(abs.(e[round(Int,L/4):round(Int,3L/4)]))/length( e[round(Int,L/4):round(Int,3L/4)])
-    if mae < siftStopThresh
-      break
-    end
+    mav = sum(abs,e[eStart:eStop])/eL
+    if mav < siftStopThresh; break; end
   return r
   end
 end
 
-function EMD(x::Vector{Float64}; emdStopThresh = 1e-2, siftStopThresh = 1e-3, includeEdge = :true)
+function EMD(x::Vector{Float64}; emdStopThresh = 1e-2, siftStopThresh = 1e-6, includeEdge = :true)
   IMF = Vector{Float64}[]
-  for _ = 1:2 #need to finish!!!!!!!!!!!!!!!
+  L = length(x); M = maximum(abs.(x))
+  eStart = round(Int,L/4); eStop = round(Int,3L/4); eL = length(eStart:eStop)
+  while true
     φ = SIFT(x, siftStopThresh=siftStopThresh, includeEdge=includeEdge)
+    if isnothing(φ); break; end
+    if maximum(abs.(φ)) > 3M; break; end
     x -= φ
     push!(IMF, φ)
+    mav = sum(abs,x[eStart:eStop])/eL
+    if mav < emdStopThresh; break; end
   end
   return push!(IMF, x)
 end
 
 function ℂSIFT(r::Vector{ComplexF64}; D = 4, siftStopThresh = 1e-3, includeEdge = :true)
+  L = length(r)
+  eStart = round(Int,L/4); eStop = round(Int,3L/4); eL = length(eStart:eStop)
+  err = false
   while true
-    L = length(r)
     e = zeros(ComplexF64,L)
     for d in 1:2D
       ϑ = 2π*(d-1)/2D
       maxIndsVals = findLocalMaxima(r, angle=ϑ, includeEdge=includeEdge)
+      if length(maxIndsVals[1]) < 4; err = true; break; end
       maxInterpolator = Dierckx.Spline1D(maxIndsVals[1], maxIndsVals[2])
       e += exp(1im*ϑ)*maxInterpolator(1:L)
     end
+    if err; break; end
     e /= 2D
     r -= e
-    mae = sum(abs.(e[round(Int,L/4):round(Int,3L/4)]))/length(e[round(Int,L/4):round(Int,3L/4)])
-    if mae < siftStopThresh
-      break
-    end
+    mav = sum(abs,e[eStart:eStop])/eL
+    if mav < siftStopThresh; break; end
   end
   return r
 end
 
 function ℂEMD(z::Vector{ComplexF64}; emdStopThresh = 1e-2, D = 4, siftStopThresh = 1e-3, includeEdge = :true)
   IMF = Vector{ComplexF64}[]
-  for _ = 1:2 #need to finish!!!!!!!!!!!!!!!
+  L = length(z); M = maximum(abs.(z))
+  eStart = round(Int,L/4); eStop = round(Int,3L/4); eL = length(eStart:eStop)
+  while true
     φ = ℂSIFT(z, D=D, siftStopThresh=siftStopThresh, includeEdge=includeEdge)
+    if maximum(abs.(φ)) > 3M; break; end
     z -= φ
     push!(IMF, φ)
+    mav = sum(abs,z[eStart:eStop])/eL
+    if mav < emdStopThresh; break; end
   end
   return push!(IMF, z)
 end
