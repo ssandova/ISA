1cdb2958e579333ced286d4a0a52f0489e44b766
Steven Sandoval <ssandova@gmail.com>
2020-03-13 16:59:20 -0600

    STFT Plots

--- test/TFDtests.jl
+++ test/TFDtests.jl
@@ -4,13 +4,37 @@ using ISA
 using Plots
 
 
-fs=40000.0
-t = collect(0.0:1/fs:1.0)
-z = exp.(1im*(2*π*10000t))
+fs=500
+t = collect(0.0:1/fs:2.0)
+
+a₀(t) = exp(-t^2)
+ω₀(t) = 50
+φ₀ = 0
+𝐶₀ = Tuple([a₀,ω₀,φ₀])
+
+a₁(t) = exp(-(t-1.0)^2)
+ω₁(t) = 2*π*100 + 150*sin(2t)
+φ₁ = π
+𝐶₁ = Tuple([a₁,ω₁,φ₁])
+
+a₂(t) = 0.8*cos(2t)
+ω₂(t) = 2*π*(-100 + 50t)
+φ₂ = π
+𝐶₂ = Tuple([a₂,ω₂,φ₂])
+
+𝑆 = [𝐶₀,𝐶₁,𝐶₂]
+z = AMFMmodel(𝑆)
+
+
+
+p1 = isaPlot3d(z, t, FreqUnits="Hz")
+
+
 windowFunction = ones(Float64,25)
 frameAdvance = 1
+zₖ = frameSignal(z(t), windowFunction, frameAdvance)
+Z,freqs = STFT(z(t), windowFunction, frameAdvance,fs=fs)
 
-zₖ = frameSignal(z, windowFunction, frameAdvance)
-Z,freqs = STFT(z, windowFunction, frameAdvance,fs=fs)
+p2 = contour(t[1:frameAdvance:end], freqs, abs.(reduce(hcat,Z)), fill=:true, seriescolor=cgrad(ISA.cmap),levels=5 )
 
-contour(t[1:frameAdvance:end], freqs, abs.(reduce(hcat,Z)), fill=:true, seriescolor=cgrad(ISA.cmap), )
+plot(p1,p2,layout=(2,1))
